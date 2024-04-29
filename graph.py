3dd41ebd8598d6c94d401aa5a00f9bd051ab3a72
Joan Massachs <22916782+JoanMassachs@users.noreply.github.com>
2024-04-29 09:53:14 +0200

    graph and plot

---
+++ graph.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import sys
+
+import networkx
+
+
+def clean_gml(x):
+    if isinstance(x, dict):
+        x = {k: clean_gml(v) for k, v in x.items()}
+        x = {k: v for k, v in x.items() if v is not None}
+        x = {k: v for k, v in x.items() if k != 'abstract_inverted_index'}
+        if len(x) > 0:
+            return x
+        else:
+            return None
+    elif isinstance(x, list):
+        x = [clean_gml(v) for v in x]
+        x = [v for v in x if v is not None]
+        if len(x) > 0:
+            return x
+        else:
+            return None
+    else:
+        return x
+
+
+def clean_graphml(x, first_level=True):
+    if isinstance(x, dict) and first_level:
+        x = {k: clean_graphml(v, first_level=False) for k, v in x.items()}
+        x = {k: v for k, v in x.items() if v is not None}
+        return x
+    elif isinstance(x, dict) and not first_level:
+        return None
+    elif isinstance(x, list):
+        return None
+    else:
+        return x
+
+
+def main(works, graph_format, path):
+    g = networkx.DiGraph()
+    for w in works:
+        refs = w['referenced_works']
+        if graph_format == 'gml':
+            w = clean_gml(w)
+        elif graph_format == 'graphml':
+            w = clean_graphml(w)
+        g.add_node(w['id'], **w)
+        for r in refs:
+            g.add_node(r)
+            if 'publication_date' in w:
+                g.add_edge(w['id'], r, date=w['publication_date'])
+            else:
+                g.add_edge(w['id'], r)
+    if graph_format == 'gml':
+        networkx.write_gml(g, path)
+    elif graph_format == 'graphml':
+        networkx.write_graphml(g, path)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'works',
+    type=argparse.FileType(), nargs='?', default=sys.stdin,
+    help='JSONL file of OpenAlex works (default: standard input)'
+)
+parser.add_argument(
+    'output',
+    type=argparse.FileType('wb'),
+    help='file to output the graph'
+)
+parser.add_argument(
+    '-f', '--format',
+    choices=['gml', 'graphml'],
+    help=(
+        'graph format to output. '
+        'GML format can store more metadata (default: gml)'
+    )
+)
+
+args = parser.parse_args()
+works = [json.loads(j) for j in args.works.read().splitlines()]
+main(works, args.format, args.output)
---
+++ plot.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def main(works, cites, per_year_of_citation, color_per_cite, path):
+    works = {w['id']: {
+        'id': w['id'],
+        'publication_year': w['publication_year']
+    } for w in works}
+
+    cites = {c['id']: {
+        'id': c['id'],
+        'publication_year': c['publication_year'],
+        'referenced_works': c['referenced_works']
+    } for c in cites}
+
+    d = dict()
+    for c in cites.values():
+        i = c['id']
+        for r in c['referenced_works']:
+            if r not in works:
+                continue
+            if per_year_of_citation:
+                y = c['publication_year']
+            else:
+                y = works[r]['publication_year']
+            if y not in d:
+                d[y] = dict()
+            if i not in d[y]:
+                d[y][i] = 0
+            d[y][i] += 1
+    d = pd.DataFrame(d)
+    d = pd.melt(d, ignore_index=False).reset_index()
+    d.columns = ['Paper', 'Year', 'n']
+    d['Year'] = d['Year'].astype(str)
+    if color_per_cite:
+        sns.set_context(rc={'patch.linewidth': 0.0})
+        f = sns.histplot(
+            d, x='Year', weights='n',
+            hue='Paper', multiple='stack'
+        )
+        plt.legend([], [], frameon=False)
+    else:
+        d = d[['Year', 'n']].groupby('Year').sum()
+        f = sns.histplot(d, x='Year', weights='n')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    f.get_figure().savefig(path)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'works',
+    type=argparse.FileType(),
+    help='JSONL file of studied works'
+)
+parser.add_argument(
+    'cites',
+    type=argparse.FileType(),
+    help='JSONL file of works that cite the studied works'
+)
+parser.add_argument(
+    'output',
+    type=argparse.FileType('wb'),
+    help='file to output the plot'
+)
+parser.add_argument(
+    '-y', '--per-year-of-citation',
+    action='store_true', default=False,
+    help=(
+        'Plot the number of citations by the year of the citation '
+        'instead of the year of the work'
+    )
+)
+parser.add_argument(
+    '-c', '--color-per-cite',
+    action='store_true', default=False,
+    help=(
+        'Plot each work that cites the studied works with a different color'
+    )
+)
+args = parser.parse_args()
+works = [json.loads(j) for j in args.works.read().splitlines()]
+cites = [json.loads(j) for j in args.cites.read().splitlines()]
+main(works, cites, args.per_year_of_citation, args.color_per_cite, args.output)
