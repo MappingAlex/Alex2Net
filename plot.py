dd73d1aa691f9c3f0d372c5c1bd33d27ee4ef66b
Joan Massachs <22916782+JoanMassachs@users.noreply.github.com>
2024-05-15 18:38:30 +0200

    plot: color citations by cited work

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import collections
 import json
 import pathlib
 
@@ -9,7 +10,7 @@ import pandas as pd
 import seaborn as sns
 
 
-def main(works, cites, per_year_of_citation, color_per_cite, ncolors, path):
+def main(works, cites, per_year_of_citation, color, ncolors, path):
     works = {w['id']: {
         'id': w['id'],
         'publication_year': w['publication_year']
@@ -21,31 +22,43 @@ def main(works, cites, per_year_of_citation, color_per_cite, ncolors, path):
         'referenced_works': [r for r in c['referenced_works'] if r in works]
     } for c in cites}
 
-    top = sorted(cites.values(), key=lambda x: -len(x['referenced_works']))
-    top = [t['id'] for t in top[:ncolors-1]]
+    if color == 'cites':
+        top = {c['id']: len(c['referenced_works']) for c in cites.values()}
+    else:
+        top = sum([c['referenced_works'] for c in cites.values()], [])
+        top = collections.Counter(top)
+    top = sorted(top.items(), key=lambda x: -x[1])
+    top = [t[0] for t in top[:ncolors-1]]
 
     d = dict()
     for c in cites.values():
-        i = c['id']
         for r in c['referenced_works']:
             if per_year_of_citation:
                 y = c['publication_year']
             else:
                 y = works[r]['publication_year']
+
             if y not in d:
                 d[y] = {t: 0 for t in top}
                 d[y]['Other'] = 0
-            if i in d[y]:
-                d[y][i] += 1
+
+            if color == 'cites':
+                t = c['id']
+            else:
+                t = r
+
+            if t in d[y]:
+                d[y][t] += 1
             else:
                 d[y]['Other'] += 1
+
     d = pd.DataFrame(d)
     d = pd.melt(d, ignore_index=False).reset_index()
     d.columns = ['Paper', 'Year', 'n']
     d['Year'] = d['Year'].astype(str)
     d = d.sort_values(by='Year')
     sns.set_context(rc={'patch.linewidth': 0.0})
-    if color_per_cite:
+    if color:
         sns.histplot(
             d, x='Year', weights='n',
             hue='Paper', multiple='stack'
@@ -84,10 +97,12 @@ parser.add_argument(
     )
 )
 parser.add_argument(
-    '-c', '--color-per-cite',
-    action='store_true', default=False,
+    '-c', '--color',
+    choices=['works', 'cites'], default=None,
     help=(
-        'Plot each work that cites the studied works with a different color'
+        'If the value is works, colors the citations by the cited work. '
+        'If the value is cites, colors the citations by the work that cites. '
+        '(default: no color)'
     )
 )
 parser.add_argument(
@@ -96,7 +111,8 @@ parser.add_argument(
     help=(
         'When -c is used, -n is the number of colors to use. '
         'If there are more works to color than colors available, '
-        'works with less references are painted with the same color.'
+        'less cited works or works that cite less '
+        'are painted with the same color.'
     )
 )
 
@@ -105,6 +121,6 @@ works = [json.loads(j) for j in args.works.read().splitlines()]
 cites = [json.loads(j) for j in args.cites.read().splitlines()]
 main(
     works, cites,
-    args.per_year_of_citation, args.color_per_cite, args.ncolors,
+    args.per_year_of_citation, args.color, args.ncolors,
     args.output
 )
